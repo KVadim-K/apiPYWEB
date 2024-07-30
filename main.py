98efac0bcca9921e22e3ff9690b40a50d208e1d8
KVadim-K <kvadimk@yandex.ru>
2024-07-30 21:03:03 +0300

    Add dz

--- main.py
+++ main.py
@@ -6,22 +6,26 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
    weather = None
+   quote = None
    news = None
    if request.method == 'POST':
        city = request.form['city']
-       #прописываем переменную, куда будет сохраняться результат и функцию weather с указанием города, который берем из формы
        weather = get_weather(city)
-       #передаем информацию о погоде в index.html
        news = get_news()
-   return render_template("index.html", weather=weather, news=news)
+       quote = get_random_quote()
+   return render_template("index.html", weather=weather, news=news, quote=quote)
+
+
 
 def get_weather(city):
-   api_key = '656ff4407a5225762cbbb0487b7b630a'
-   #адрес, по которомы мы будем отправлять запрос. Не забываем указывать f строку.
+   api_key = "656ff4407a5225762cbbb0487b7b630a"
    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-   #для получения результата нам понадобится модуль requests
    response = requests.get(url)
-   #прописываем формат возврата результата
+   return response.json()
+
+def get_random_quote():
+   url = "https://api.quotable.io/random"
+   response = requests.get(url)
    return response.json()
 
 def get_news():
